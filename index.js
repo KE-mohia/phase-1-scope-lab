5a4d87f57c3c30d6c13df9be18d4f9ace4ad596e
wayne <keyawayne1@gmail.com>
2023-06-20 00:27:08 +0300

    assinment complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+// modifies upperCaseCustomerName
+function upperCaseCustomerName(){
+customerName = customerName.toUpperCase();
+}
+
+// setBestCustomer
+function setBestCustomer(){
+bestCustomer = "not bob";
+}
+
+// overwrites the best customer
+function overwriteBestCustomer(newBestCustomer){
+bestCustomer = newBestCustomer;
+}
+
+// delare a constant called leasFavoriteCustomer in global scope
+const leasFavoriteCustomer = "somebody";
+
+// unsuccessfully tries to reasign the leest favorite customer
+function changeLeastFavoriteCustomer(){
+    leasFavoriteCustomer ="nobody";
+}
